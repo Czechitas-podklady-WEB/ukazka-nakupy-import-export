e6c169900c7122291a0935840a83c0cb76768407
Martin Podloucký <podlouckymartin@gmail.com>
2023-10-07 11:51:15 +0200

    Kod aplikace po import/export

---
+++ index.js
@@ -0,0 +1,17 @@
+import { addItem } from './shoplist.js';
+import { renderList } from './render.js';
+
+const addBtn = document.querySelector('.btn-add');
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const nameInput = document.querySelector('#input-name');
+  const amountInput = document.querySelector('#input-amount');
+  addItem(nameInput.value, amountInput.value);
+  renderList();
+
+  nameInput.value = '';
+  amountInput.value = '';
+});
+
+renderList();
